499af7c4e6155304def4dd8cf10dfe04f0e84927
Shaun Gill <shaung75@gmail.com>
2018-08-15 22:57:53 +0100

    changing to 3 column shop layout and some styling stuff

--- wp-content/themes/daddycool/functions.php
+++ wp-content/themes/daddycool/functions.php
@@ -15,4 +15,13 @@ function green_farm_child_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'green_farm_child_enqueue_styles', 11);
-?>
+
+/**
+ * Change number or products per row to 3
+ */
+add_filter('loop_shop_columns', 'loop_columns');
+if (!function_exists('loop_columns')) {
+  function loop_columns() {
+    return 3; // 3 products per row
+  }
+}
